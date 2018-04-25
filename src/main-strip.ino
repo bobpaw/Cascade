ef5cea27489eb45d1b5e5def862dfb85038e04fe
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-04-25 18:41:56 -0500

    Added test using strip instead of matrix
    Added clear to beginning to test
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- src/main-strip.ino
+++ src/main-strip.ino
@@ -1,11 +1,5 @@
 #include <Arduino.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_NeoPixel.h>
-#include <Adafruit_NeoMatrix.h>
-
-// Required for GFX on PlatformIO
-#include <SPI.h>
-
 #include <string.h>
 #include <stdlib.h>
 
@@ -15,10 +9,17 @@
 
 byte * boardmap = NULL; // Internal map of values
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(WIDTH, HEIGHT, 3,
-                                               NEO_MATRIX_TOP + NEO_MATRIX_RIGHT +
-                                               NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-                                               NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(WIDTH, HEIGHT, 3, NEO_GRB + NEO_KHZ800);
+
+uint16_t location (int x, int y) {
+  uint16_t result = HEIGHT * WIDTH;
+  if (y % 2 == 0) {
+    return uint16_t(HEIGHT * WIDTH - (HEIGHT * x + y));
+    result -= HEIGHT * x + y;
+  } else {
+    return uint16_t(HEIGHT * WIDTH - (HEIGHT * x + (HEIGHT - y)));
+  }
+}
 
 uint16_t top_color = 0; // Stored in boardmap as 1
 uint16_t middle_color = 0; // Stored in boardmap as 2
@@ -33,22 +34,21 @@ uint8_t add_count = 0; // Number to add each pass
 void setup () {
   boardmap = (byte*) malloc(WIDTH * HEIGHT); // Use malloc to initialize
   memset(boardmap, 0, WIDTH * HEIGHT);
-  matrix.begin();
-  matrix.setBrightness(10);
+  strip.begin();
+  strip.setBrightness(10);
   Serial.begin(115200);
 
   // Colors for rain, stolen from old code
   // uint16_t Color(uint8_t r, uint8_t g, uint8_t b);
-  top_color = matrix.Color(71, 116, 255);
-  middle_color = matrix.Color(51, 106, 72);
-  bottom_color = matrix.Color(71, 116, 156);
-  black = matrix.Color(0, 0, 0);
-  error_color = matrix.Color(10, 200, 50);
+  top_color = strip.Color(71, 116, 255);
+  middle_color = strip.Color(51, 106, 72);
+  bottom_color = strip.Color(71, 116, 156);
+  black = strip.Color(0, 0, 0);
+  error_color = strip.Color(10, 200, 50);
 }
 
 void loop () {
-  matrix.clear()
-
+  strip.clear();
   // Move everything down one space internally
   for (int y = HEIGHT - 1; y > 0; y--) {
     for (int x = 0; x < WIDTH; x++) {
@@ -91,9 +91,9 @@ void loop () {
         color = error_color;
         break;
       }
-      matrix.drawPixel(x, y, color);
+      strip.setPixelColor(location(x, y), color);
     }
   }
-  matrix.show();
+  strip.show();
   delay(100);
 }
